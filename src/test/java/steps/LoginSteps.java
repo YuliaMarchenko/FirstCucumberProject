2e11e06876f1258d351004ee75391932e2d8b023
Yulia Marchenko <yuyakovleva11@gmail.com>
2022-11-03 11:48:23 +0100

    ...

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -1,14 +1,20 @@
 package steps;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class LoginSteps {
@@ -50,12 +56,18 @@ public class LoginSteps {
     }
 
     @And("Enter valid email and invalid password")
-    public void enterValidEmailAndInvalidPassword(){
-
+    public void enterValidEmailAndInvalidPassword(DataTable table){
+        List<Map<String, String>> dataTable = table.asMaps();
+        String email = dataTable.get(0).get("email");
+        String password = dataTable.get(1).get("password");
+        type(By.xpath("//input[@placeholder='Email']"), email);
+        type(By.xpath("//input[@placeholder='Password']"), password);
     }
 
     @Then("Alert appeared")
-    public void isAlertPresent(){}
+    public void isAlertAppeared(){
+        Assert.assertTrue(isAlertPresent());
+    }
 
     private void click(By locator){
         driver.findElement(locator).click();
@@ -71,5 +83,16 @@ public class LoginSteps {
             driver.findElement(locator).clear();
             driver.findElement(locator).sendKeys(text);
         }
+    }
+
+    private boolean isAlertPresent() {
+        Alert alert = new WebDriverWait(driver, 20).until(ExpectedConditions.alertIsPresent());
+        if (alert == null){
+            return false;
+        } else {
+            driver.switchTo().alert();
+            alert.accept();
+        }
+        return true;
     }
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -1,10 +1,12 @@
 Feature: Login
 
+  @openLoginPage
   Scenario: Open Login Page
     Given Navigate to Home Page
     When Click on Login tab
     Then Appear LoginRegistration form
 
+  @validData
   Scenario: Login with valid date
     Given Navigate to Home Page
     When Click on Login tab
@@ -12,14 +14,16 @@ Feature: Login
     And Click on login Button
     Then SingOut button displayed
 
+  @invalidPassword
   Scenario Outline: Login with invalid password
     Given Navigate to Home Page
     When Click on Login tab
     And Enter valid email and invalid password
-    |email|password|
-    |<email>|<password>
+      | email   | password  |
+      | <email> | <password> |
     And Click on login Button
     Then Alert appeared
     Examples:
-      | test102@test.com | 123 |
+      | email   | password  |
+      | test102@test.com | 123        |
       | test102@test.com | test12_TES |
